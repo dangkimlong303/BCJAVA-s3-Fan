fda324c2d98f503d50b1271e13c616e9ef4ac5f3
dangkimlong303 <dangkimlong303@gmail.com>
2018-11-01 14:27:19 +0700

    fan

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Fan fan1 = new Fan();
+        Fan fan2 = new Fan();
+        fan2.setOn(false);
+        fan2.setSpeed(fan2.getMEDIUM());
+        fan2.setRadius(5);
+        fan2.setColor("Blue");
+        fan1.setOn(true);
+        fan1.setSpeed(fan1.getFAST());
+        fan1.setRadius(10);
+        fan1.setColor("Yellow");
+        System.out.println(fan1.display());
+
+        System.out.println(fan2.display());
+    }
+}
